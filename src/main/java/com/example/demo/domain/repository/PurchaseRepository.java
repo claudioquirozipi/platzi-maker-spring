e29b9e0ac22a72318d5c35881f21be24bc799395
Claudio Quiroz <claudioquirozipi@gmail.com>
2021-02-11 10:42:49 -0500

    bug

---
+++ src/main/java/com/example/demo/domain/repository/PurchaseRepository.java
@@ -0,0 +1,12 @@
+package com.example.demo.domain.repository;
+
+import com.example.demo.domain.Purchase;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PurchaseRepository {
+    List<Purchase> getAll();
+    Optional<List<Purchase>> getByClient(String clientId);
+    Purchase save(Purchase purchase);
+}
